de65029276fbedd59c31a7e43c8640f172715ed0
aryankaushik21 <aryankaushik207@gmail.com>
2024-09-02 19:19:17 +0530

    changed port

--- app.py
+++ app.py
@@ -60,5 +60,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
